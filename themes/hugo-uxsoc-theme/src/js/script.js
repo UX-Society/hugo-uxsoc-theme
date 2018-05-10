bb4f90d14d9b814291f9655e674b927d9c5dae69
Xavi Ablaza <xlablaza@gmail.com>
2018-05-10 00:59:08 -0700

    Add /hero to test navbar

--- themes/hugo-uxsoc-theme/src/js/script.js
+++ themes/hugo-uxsoc-theme/src/js/script.js
@@ -1,1 +1,17 @@
 import 'bootstrap'
+
+$(document).ready(function () {
+    $(".trigger").on("click", function () {
+        $(".trigger").each(function () {
+            $(this).toggleClass("on")
+        }), $(".menu").fadeToggle(200), $(this).hasClass("on") ? $("body").bind("mousewheel touchmove", function (e) {
+            e.preventDefault()
+        }) : $("body").unbind("mousewheel touchmove")
+    })
+})
+
+var lastScrollTop = 0;
+$(window).on("scroll", function () {
+    var st = $(this).scrollTop();
+    st > lastScrollTop ? $(".floating-nav").addClass("is-hidden") : $(window).scrollTop() > 200 ? $(".floating-nav").removeClass("is-hidden") : $(".floating-nav").addClass("is-hidden"), lastScrollTop = st, 0 == $(this).scrollTop() && $(".floating-nav").addClass("is-hidden")
+})
